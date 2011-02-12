1a201cf5268bd6b1110416b26c050959a2412b31
bifferos <bifferos@yahoo.co.uk>
2011-02-12 18:49:45 +0000

    Allow biffconfig to print specific config values only, make it verify the config checksum and print a warning when it's wrong.

--- package/biffconfig/src/biffconfig.c
+++ package/biffconfig/src/biffconfig.c
@@ -54,43 +54,263 @@ struct _cfg
 struct _cfg g_vals;
 
 
-// read the config
-void config_init()
+// get the md5, write it to the given buffer.
+void config_get_md5(md5_byte_t* digest)
 {
-  int fd = open("/dev/mtd0", O_RDONLY);
-  lseek(fd, 0x4000, SEEK_SET);
-  read(fd, &g_vals, sizeof(struct _cfg));
+  md5_state_t pms;
+  
+  md5_init(&pms);
+  md5_append(&pms, (char*)&g_vals, 0x1ff0);
+  md5_finish(&pms, digest);  
 }
 
 
+void print_md5(md5_byte_t* d)
+{
+  int i;
+  for (i=0;i<16;i++)
+  {
+    printf("%02x", d[i]);
+  }
+}
+
+
+// read the config
+void config_init()
+{
+  md5_byte_t digest[16];
+  int found_term, i;
+  int fd = open("/dev/mtd0", O_RDONLY);
+  lseek(fd, 0x4000, SEEK_SET);
+  read(fd, &g_vals, sizeof(struct _cfg));
+
+  config_get_md5(digest);
+
+  if (memcmp(g_vals.digest, digest, sizeof(digest)) !=0)
+  {
+    printf("WARNING: Configuration md5 mismatch, configuration may have been corrupted\n");
+    printf("Expected: ");  print_md5(digest);  printf("\n");
+    printf("Actual:   ");    print_md5(g_vals.digest);  printf("\n");
+  }
+  
+  found_term = 0;
+  for (i=0;i<sizeof(g_vals.val.cmndline);i++)
+  {
+    if (!g_vals.val.cmndline[i]) 
+    {
+      found_term = 1;
+      break;
+    }
+  }
+  if (!found_term)
+  {
+    printf("Kernel command-line lacks termination, configuration may have been corrupted\n");
+    // add some termination
+    g_vals.val.cmndline[sizeof(g_vals.val.cmndline)-1] = 0;
+  }
+}
+
+
+typedef struct _cfg_param
+{
+  char* name;
+  int index;
+  char* help;
+} cfg_param_t;
+
+#define CFG_BOOTSOURCE 0
+#define CFG_CONSOLE 1
+#define CFG_NIC 2
+#define CFG_BOOTTYPE 3
+#define CFG_LOADADDRESS 4
+#define CFG_CMNDLINE 5
+#define CFG_KERNELMAX 6
+
+
+cfg_param_t g_params[] = {
+ { "bootsource", CFG_BOOTSOURCE, "flash|MMC|Net" },
+ { "console", CFG_CONSOLE, "disabled|enabled" },
+ { "nic", CFG_NIC, "disabled|enabled|promiscuous" },
+ { "boottype", CFG_BOOTTYPE, "flat binary|Linux|Multiboot|Coreboot" },
+ { "loadaddress", CFG_LOADADDRESS, "<RAM address>" },
+ { "cmndline", CFG_CMNDLINE, "<string>" },
+ { "kernelmax", CFG_KERNELMAX, "<count of 64k sectors>" },
+ { NULL },
+};
+
+
+// Print out one of the config values in human-readable form
+char* render(int index)
+{
+  static char buffer[sizeof(g_vals.val.cmndline)];
+  switch (index)
+  {
+  	case CFG_BOOTSOURCE:
+  	  switch (g_vals.val.bootsource)
+  	  {
+	     case 0: return "flash";
+	     case 1: return "MMC";
+	     case 2: return "Net";
+	     default:
+	       return "Invalid";
+          }
+        case CFG_CONSOLE:
+  	  switch (g_vals.val.console)
+  	  {
+	     case 0: return "disabled";
+	     case 1: return "enabled";
+	     default:
+	       return "Invalid";
+          }
+          break;
+        case CFG_NIC:
+  	  switch (g_vals.val.nic)
+  	  {
+	     case 0: return "disabled";
+	     case 1: return "enabled";
+	     case 2: return "promiscuous";
+	     default:
+	       return "Invalid";
+          }
+          break;
+        case CFG_BOOTTYPE:
+  	  switch (g_vals.val.boottype)
+  	  {
+	     case 0: return "flat binary";
+	     case 1: return "Linux";
+	     case 2: return "Multiboot";
+	     case 3: return "Coreboot";
+	     default:
+	       return "Invalid";
+          }
+        case CFG_LOADADDRESS:
+          sprintf(buffer, "0x%x", g_vals.val.loadaddress);
+          break;
+        case CFG_CMNDLINE:
+          sprintf(buffer, "%s", g_vals.val.cmndline);
+          break;
+        case CFG_KERNELMAX:
+          sprintf(buffer, "0x%x", g_vals.val.kernelmax);
+          break;
+        default:
+          strcpy(buffer, "<INVALID>");          
+  }
+  return buffer;  
+}
+
+
+// Print all the config values
 void config_print()
 {
+  cfg_param_t* param;
   printf("Current configuration:\n");
-  printf("  bootsource: %d\n", g_vals.val.bootsource);
-  printf("  console: %d\n", g_vals.val.console);
-  printf("  nic: %d\n", g_vals.val.nic);
-  printf("  boottype: %d\n", g_vals.val.boottype);
-  printf("  cmndline: %s\n", g_vals.val.cmndline);
-  printf("  kernelmax: %d\n", g_vals.val.kernelmax);
+  
+  param = g_params;
+  while (param->name)
+  {
+    printf("  %s: %s\n", param->name, render(param->index));
+    param++;
+  }
+}
+
+
+// Print one config values
+int config_print_one(char* param)
+{
+  cfg_param_t* p;
+  int found;
+  
+  p = g_params;
+  found = 0;
+  while (p->name)
+  {
+    if (strcmp(p->name, param)==0)
+    {
+      printf("%s\n", render(p->index));
+      found = 1;
+      break;
+    }
+    p++;
+  }
+  return found;
+}
+
+
+// Print one config values
+int config_set_one(char* param, char* value)
+{
+  cfg_param_t* p;
+  int found;
+  
+  p = g_params;
+  found = 0;
+  while (p->name)
+  {
+    if (strcmp(p->name, param)==0)
+    {
+      // set the value p->index.
+      printf("TODO: Setting values is unsupported\n");
+      found = 1;
+      break;
+    }
+    p++;
+  }
+  return found;
 }
 
 
 #define DEVICE "/dev/mtd0"
 
 
+void help()
+{
+  printf("Usage: biffconfig [set|get] <parameter> [value]\n");
+  _exit(-1);
+}
+
+
 int main(int argc, char *argv[])
 {
+  int ok;
 	config_init();
-	if (argc == 1)  // no arguments, just print the values
+	switch (argc)
 	{
-		
+	  case 1:
 		config_print();
+		break;
+	  case 2:
+	  	help();
+	  case 3:
+	  	if (strcmp(argv[1],"get")==0)
+	  	{
+		  ok = config_print_one(argv[2]);
+		  if (!ok)
+		  {
+		    printf("Unrecognised option '%s'\n", argv[2]);
+		  }
+	  	}
+	  	else
+	  	{
+	  	  help(); // need three parameters for a set operation
+	  	}
+	  	break;
+	  case 4:
+	  	if (strcmp(argv[1],"set")==0)
+	  	{
+		  ok = config_set_one(argv[2], argv[3]);
+		  if (!ok)
+		  {
+		    printf("Unrecognised option '%s'\n", argv[2]);
+		  }
+	  	}
+	  	else
+	  	{
+	  	  help(); // get needs one less parameter.
+	  	}	  
+	        break;
+	  default:
+	    help();
 	}
-	else
-	{
-	   printf("Setting values unsupported in this version\n");
-	}
-
 	return 0;
 }
 
